5172f66fccdb8234119bbb558368d1136a25de5a
csupreme19 <csupreme19@gmail.com>
2023-03-02 15:59:26 +0900

    @AutoConfiguration과 ImportSelector

---
+++ src/test/java/hello/selector/ImportSelectorTest.java
@@ -0,0 +1,36 @@
+package hello.selector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ImportSelectorTest {
+
+    @Test
+    void staticConfig() {
+        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(StaticConfig.class);
+        HelloBean bean = appContext.getBean(HelloBean.class);
+        assertThat(bean).isNotNull();
+    }
+
+    @Test
+    void selectorConfig() {
+        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(SelectorConfig.class);
+        HelloBean bean = appContext.getBean(HelloBean.class);
+        assertThat(bean).isNotNull();
+    }
+
+    @Configuration
+    @Import(HelloConfig.class)
+    static class StaticConfig {
+    }
+
+    @Configuration
+    @Import(HelloImportSelector.class)
+    static class SelectorConfig {
+    }
+
+}
